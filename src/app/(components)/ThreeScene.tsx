fa33d0a6d5fbe03b4e9a7286b20430a5366cf5f4
Bryant Wong <bryanthwong@gmail.com>
2024-01-16 22:24:03 -0500

    testing blocks

--- src/app/(components)/ThreeScene.tsx
+++ src/app/(components)/ThreeScene.tsx
@@ -6,8 +6,12 @@ const ThreeScene= () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const rotationRef = useRef<number>(0.01);
   const birdDirectionRef = useRef<number>(-1);
+  let numberOfPillars = 5
+
   let birdUpPeakIncrease = 0
-  let birdUpPeakIncreasePeak = 100
+  let birdUpPeakIncreasePeak = 0.5
+  let birdUpPeakIncreaseNumerator = 0.05
+  let decreaseAmount = 0.01
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -22,34 +26,85 @@ const ThreeScene= () => {
       containerRef.current?.appendChild(renderer.domElement)
       camera.position.z = 5
 
+      // Get the field of view
+      var fov = camera;
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1)
-      // const material = new THREE.MeshBasicMaterial( { color: 0xff0000})
-      // const texture = new THREE.TextureLoader().load( "textures/water.jpg" );
-      const material = new THREE.MeshNormalMaterial({blendColor: 0xff0000, flatShading:true})
+      // Log the field of view to the console
+      console.log("Field of View:", fov);
+
+      const visibleHeightAtZDepth = ( depth, camera ) => {
+        // compensate for cameras not positioned at z=0
+        const cameraOffset = camera.position.z;
+        if ( depth < cameraOffset ) depth -= cameraOffset;
+        else depth += cameraOffset;
       
-      const cube = new THREE.Mesh(geometry, material)
+        // vertical fov in radians
+        const vFOV = camera.fov * Math.PI / 180; 
+      
+        // Math.abs to ensure the result is always positive
+        return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+      };
+      
+      const visibleWidthAtZDepth = ( depth, camera ) => {
+        const height = visibleHeightAtZDepth( depth, camera );
+        return height * camera.aspect;
+      };
+
+      const vHeight = visibleHeightAtZDepth(0.5,camera)
+
+      console.log('VISIBILE',vHeight)
+
+
+      const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+      const cubeMaterial = new THREE.MeshNormalMaterial({blendColor: 0xff0000, flatShading:true})
+      
+      const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
       scene.add(cube)
+
+      const pillarCubesArrTop = [...Array(numberOfPillars).keys()].map(idx=>{
+        const height = parseFloat(((Math.random()*2)+0.5).toFixed(2))
+        const pillarCubeGeometry = new THREE.BoxGeometry(1, height, 1)
+        const pillarCubeMaterial = new THREE.MeshNormalMaterial({blendColor: 0xff0000, flatShading:true})
+        
+        const pillarCube = new THREE.Mesh(pillarCubeGeometry, pillarCubeMaterial)
+
+        pillarCube.position.y = vHeight/2 - (height/2)
+        //pillarCube.position.y = 3.43
+        pillarCube.position.x = idx%2===0?idx * 1.2:(idx*1.2)*-1
+        pillarCube.position.z = 0
+        pillarCube.geometry.computeBoundingBox()
+        console.log('geometry',pillarCube.geometry.boundingBox)
+
+        console.log('poistuin',pillarCube.position.x)
+        //pillarCube.position.x = -0.2
+        scene.add(pillarCube)
+        return pillarCube
+      })
+
       renderer.render(scene, camera)
       console.log('po',cube.position)
-      geometry.computeBoundingBox()
-      console.log('geometry',geometry.boundingBox)
+      // cube.geometry.computeBoundingBox()
+      // console.log('geometry',cube.geometry.boundingBox)
 
       // Add this function inside the useEffect hook
+
       console.log('cube-posiiton',cube.position.x,cube.position.y)
       let stop = false
 
       const renderScene = () => {
         if(stop) return
-        // cube.rotation.x += rotationRef.current
-        // cube.rotation.y += rotationRef.current
+
+        cube.rotation.x += rotationRef.current
+        cube.rotation.y += rotationRef.current
         if(birdDirectionRef.current>0){
-          cube.position.y += 0.2/(birdUpPeakIncrease+1)
-          birdUpPeakIncrease+=1
+          console.log('birdUpPeakIncrease',birdUpPeakIncrease)
+          cube.position.y += birdUpPeakIncreaseNumerator/(birdUpPeakIncrease+1)
+          birdUpPeakIncrease+=0.01
         }else{
-          cube.position.y -= 0.01
+          cube.position.y -= decreaseAmount
         }
-        console.log('birdUpPeakIncrease',birdUpPeakIncrease)
+  
         if(birdUpPeakIncrease>=birdUpPeakIncreasePeak){
           birdDirectionRef.current=-1
           birdUpPeakIncrease=0
@@ -61,8 +116,10 @@ const ThreeScene= () => {
 
         const pos = new THREE.Vector3(cube.position.x+1, cube.position.y+(1*birdDirectionRef.current), cube.position.z);
         if (!frustum.containsPoint(pos)) {
-            console.log('Out of view',pos)
-            stop = true
+            // console.log('Out of view',pos)
+            decreaseAmount = 0
+            //birdDirectionRef.current=(-1*birdDirectionRef.current)
+            //stop = true
         }
 
         renderer.render(scene, camera)
@@ -98,6 +155,7 @@ const ThreeScene= () => {
       // rotationRef.current+=0.01
       birdDirectionRef.current = 1
       birdUpPeakIncrease = 0
+      decreaseAmount = 0.01
     }}>
       test
     </button>
